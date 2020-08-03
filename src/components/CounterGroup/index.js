7ed7212d04725d2965a473ecb22f1f99bc313059
skymike <skymike11@qq.com>
2020-08-03 22:00:07 +0800

    [react-test][sam] feature : use store to save and fetch

--- src/components/CounterGroup/index.js
+++ src/components/CounterGroup/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import Counter from "../Counter";
+import store from "../store/index"
+import {SETSIZE} from "../store/actionTypes";
 
 class CounterGroup extends React.Component {
-
     constructor(props) {
         super(props);
         this.state = {
             size: 0,
-            totalNum: 0
-        }
+            totalNum: store.getState().totalNum
+        };
+        this.storeChange = this.storeChange.bind(this);
+        store.subscribe(this.storeChange);
+    }
+
+    storeChange() {
+        this.setState({
+            totalNum: store.getState().totalNum
+        })
     }
 
     handleResize = (event) => {
@@ -19,19 +28,8 @@ class CounterGroup extends React.Component {
             this.setState({
                 size: event.target.value ? parseInt(event.target.value) : 0,
             });
+            store.dispatch({type: SETSIZE, size: event.target.value})
         }
-    };
-
-    handleIncrease = () => {
-        this.setState(preState => ({
-            totalNum: preState.totalNum + 1
-        }))
-    };
-
-    handleReduce = () => {
-        this.setState(preState => ({
-            totalNum: preState.totalNum - 1
-        }))
     };
 
     render() {
